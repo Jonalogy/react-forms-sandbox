28c2f325cfd71c1626aafeff1d05466252f700c3
Jonathan Tan <jontan@palo-it.com>
2020-06-05 15:28:40 +0800

    Add notes to Brochure typing

--- src/layouts/brochure/Brochure.tsx
+++ src/layouts/brochure/Brochure.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Brochure.scss';
 
-type IBrochure = Record<string, any>;
+type IBrochure = Record<string, any>; // Eslint suggests that to use [Record<>] to represent the idea of any object (https://www.typescriptlang.org/docs/handbook/utility-types.html#recordkt)
 export const Brochure: React.FC<IBrochure> = (props) => {
     return (
         <div className={'layout'}>
