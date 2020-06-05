e38f11b89fae5b58312d19fbd5feaef289b90c0c
Jonathan Tan <jontan@palo-it.com>
2020-06-05 21:54:37 +0800

    Add eslint react-hook plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,9 @@ module.exports = {
       jsx: true // Allows for the parsing of JSX
     }
   },
+  plugins: [
+    "react-hooks"
+  ],
   extends: [
     "plugin:react/recommended", // Uses the recommended rules from @eslint-plugin-react
     "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
@@ -14,6 +17,8 @@ module.exports = {
     "plugin:prettier/recommended" // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
   rules: {
+    "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
+    "react-hooks/exhaustive-deps": "warn", // Checks effect dependencies,
     "react/prop-types": 0 // To drop requirement of prop-types check (https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/prop-types.md)
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     // e.g. "@typescript-eslint/explicit-function-return-type": "off",
