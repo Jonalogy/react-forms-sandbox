07448c5b581400ab82643358222e923b2a7e787d
Jonathan Tan <jontan@palo-it.com>
2020-06-05 15:25:26 +0800

    Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,13 @@ module.exports = {
     "plugin:prettier/recommended" // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
   rules: {
+    "react/prop-types": 0 // To drop requirement of prop-types check (https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/prop-types.md)
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+  },
+  settings: {
+    "react": {
+      "version": "detect" // To remove error on React version not being specified (https://github.com/yannickcr/eslint-plugin-react/issues/2157)
+    }
   }
 };
