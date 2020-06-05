15cef7bfdc94ddc2e7431e30385a3e59068bb6f6
Jonathan Tan <jontan@palo-it.com>
2020-06-05 19:34:01 +0800

    Update radio button label htmlFor attribute

--- src/forms/fields/RadioButtons.tsx
+++ src/forms/fields/RadioButtons.tsx
@@ -9,6 +9,7 @@ type TRadioOptions = {
 
 interface IRadioButtons {
     fieldName: string;
+    options: TRadioOptions[];
     register: (ref: Element | null) => void;
     options: TRadioOptions[];
 }
@@ -17,10 +18,10 @@ const RadioButtons: React.FC<IRadioButtons> = (props) => {
         <section className="radioGroup">
             {props.options.map((o, idx) => (
                 <React.Fragment key={`${props.fieldName}_${idx}`}>
-                    <label htmlFor={props.fieldName}>
+                    <label htmlFor={`${props.fieldName}_${idx}`}>
                         <input
                             type="radio"
-                            id={props.fieldName}
+                            id={`${props.fieldName}_${idx}`}
                             name={props.fieldName}
                             ref={props.register}
                             value={o.value}
