f1e88b3966768c97a658953305dbe3badc819c12
Jonathan Tan <jontan@palo-it.com>
2020-06-05 15:15:48 +0800

    Update ReactHookForm with custom validation

--- src/views/reactHookForm/ReactHookForm.tsx
+++ src/views/reactHookForm/ReactHookForm.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { useForm, Controller, EventFunction } from 'react-hook-form';
+import { useForm, Controller } from 'react-hook-form';
 import './ReactHookForm.scss';
 
 enum Fields {
-    name = 'name',
-    age = 'age',
-    hobbies = 'hobbies',
+    name = 'Name',
+    age = 'Age',
+    hobbies = 'Hobbies',
 }
 
 type Inputs = {
@@ -19,38 +19,50 @@ export function ReactHookForm() {
         Inputs
     >();
     const onSubmit = (data) => console.log('submitted:', data);
-    console.log(watch(Fields.hobbies));
-    console.log(errors);
-    
+
+    /* Do not trigger validation for registered uncontrolled components here.
+       Every call to [triggerValidation] forces a re-render
+    */
+    triggerValidation(Fields.hobbies);
+
     return (
         <div className="frame">
             <form className="form" onSubmit={handleSubmit(onSubmit)}>
-                {/* register your input into the hook by invoking the "register" function */}
                 <input name={Fields.name} placeholder="Name" ref={register({ required: true })} />
 
-                {/* include validation with required or other standard HTML validation rules */}
                 <input
                     name={Fields.age}
-                    disabled={!watch(Fields.name)}
+                    // disabled={!watch(Fields.name)}
                     placeholder="Age"
-                    ref={register({ required: true })}
+                    ref={register({ required: `${Fields.age} is required` })}
                     onChange={() => triggerValidation(Fields.age)}
                 />
-                {/* errors will return when field validation fails  */}
-                {errors[Fields.age] && <span className="errorMsg">This field is required</span>}
+                {errors[Fields.age] && <span className="errorMsg">{errors[Fields.age]!.message}</span>}
+
+                {/* Controlled Component's validation
+                    - Unlike uncontrolled components, controlled components are assumed to have their own validation
+                    - To rely on [rules] for validation, one has to call it intentionally (For example: on form re-render)
+                */}
                 <Controller
                     as={<textarea />}
-                    disabled={!getValues(Fields.age)}
+                    // disabled={!getValues(Fields.age)}
                     name={Fields.hobbies}
                     control={control}
-                    rules={{ required: true }}
+                    rules={{
+                        required: `Please list your hobbies ${Fields.hobbies}`,
+                        validate: {
+                            noSpecialChar: (val) =>
+                                /\W+/.test(val) ? `${Fields.hobbies} cannot contain special characters` : undefined,
+                            minStrLength: (val) =>
+                                String(val).length < 5 ? 'At least 5 characters please' : undefined,
+                        },
+                    }}
                     onChange={([event]) => {
-                      if(!event.target.value) { 
-                        return
-                      }
-                      return String(event.target.value).replace(/\d/g, '')
+                        console.log('RHF:', event.target.value);
+                        return event.target.value.replace(/\d/g, '');
                     }}
                 />
+                {errors[Fields.hobbies] && <span className="errorMsg">{errors[Fields.hobbies]!.message}</span>}
 
                 <input type="submit" />
             </form>
